d5ce5099f03f103753a3fe6d53ee43f21aedc3d4
Trent Schafer <trent.schafer@gmail.com>
2020-05-21 18:55:28 +1000

    Replace system with admin

--- mksample.sql
+++ mksample.sql
@@ -3,8 +3,8 @@ Rem $Header: rdbms/demo/schema/mksample.sql.sbs /main/12 2015/03/19 10:23:26 smt
 Rem
 Rem mksample.sql
 Rem
-Rem Copyright (c) 2001, 2016, Oracle and/or its affiliates.  All rights reserved. 
-Rem 
+Rem Copyright (c) 2001, 2016, Oracle and/or its affiliates.  All rights reserved.
+Rem
 Rem Permission is hereby granted, free of charge, to any person obtaining
 Rem a copy of this software and associated documentation files (the
 Rem "Software"), to deal in the Software without restriction, including
@@ -12,10 +12,10 @@ Rem without limitation the rights to use, copy, modify, merge, publish,
 Rem distribute, sublicense, and/or sell copies of the Software, and to
 Rem permit persons to whom the Software is furnished to do so, subject to
 Rem the following conditions:
-Rem 
+Rem
 Rem The above copyright notice and this permission notice shall be
 Rem included in all copies or substantial portions of the Software.
-Rem 
+Rem
 Rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 Rem EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 Rem MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -39,14 +39,14 @@ Rem      - OUI instantiates this script during install and saves it
 Rem        as mksample.sql. The instantiated scripts matches
 Rem        the directory structure on your system
 Rem      - Tablespace EXAMPLE created with:
-Rem		CREATE TABLESPACE example 
-Rem		    NOLOGGING 
-Rem		    DATAFILE '<filename>' SIZE 150M REUSE 
+Rem		CREATE TABLESPACE example
+Rem		    NOLOGGING
+Rem		    DATAFILE '<filename>' SIZE 150M REUSE
 Rem		    AUTOEXTEND ON NEXT 640k
 Rem                 MAXSIZE UNLIMITED
 Rem		    EXTENT MANAGEMENT LOCAL
 Rem                 SEGMENT SPACE MANAGEMENT AUTO;
-Rem                 
+Rem
 Rem      - CAUTION: This script will erase the following schemas:
 Rem        - HR
 Rem        - OE
@@ -56,7 +56,7 @@ Rem        - IX
 Rem	   - BI
 Rem      - CAUTION: Never use the above mentioned Sample Schemas for
 Rem        anything other than demos and examples
-Rem      - USAGE: To return the Sample Schemas to their initial 
+Rem      - USAGE: To return the Sample Schemas to their initial
 Rem        state, you can call this script and pass the passwords
 Rem        for SYS, SYSTEM and the schemas as parameters.
 Rem        Example: @/your/path/to/mksample mgr secure h1 o2 p3 q4 s5
@@ -98,42 +98,39 @@ SET ECHO OFF
 SET CONCAT '.'
 SET SHOWMODE OFF
 
-PROMPT 
+PROMPT
 PROMPT specify password for SYSTEM as parameter 1:
-DEFINE password_system     = &1
-PROMPT 
-PROMPT specify password for SYS as parameter 2:
-DEFINE password_sys        = &2
-PROMPT 
-PROMPT specify password for HR as parameter 3:
-DEFINE password_hr         = &3
+DEFINE password_admin     = &1
 PROMPT
-PROMPT specify password for OE as parameter 4:
-DEFINE password_oe         = &4
+PROMPT specify password for HR as parameter 2:
+DEFINE password_hr         = &2
 PROMPT
-PROMPT specify password for PM as parameter 5:
-DEFINE password_pm         = &5
+PROMPT specify password for OE as parameter 3:
+DEFINE password_oe         = &3
 PROMPT
-PROMPT specify password for IX as parameter 6:
-DEFINE password_ix         = &6
+PROMPT specify password for PM as parameter 4:
+DEFINE password_pm         = &4
 PROMPT
-PROMPT specify password for  SH as parameter 7:
-DEFINE password_sh         = &7
-PROMPT 
-PROMPT specify password for  BI as parameter 8:
-DEFINE password_bi         = &8
-PROMPT 
-PROMPT specify default tablespace as parameter 9:
-DEFINE default_ts          = &9
+PROMPT specify password for IX as parameter 5:
+DEFINE password_ix         = &5
 PROMPT
-PROMPT specify temporary tablespace as parameter 10:
-DEFINE temp_ts             = &10
-PROMPT 
-PROMPT specify log file directory (including trailing delimiter) as parameter 11:
-DEFINE logfile_dir         = &11
-PROMPT 
-PROMPT specify connect string as parameter 12:
-DEFINE connect_string     = &12
+PROMPT specify password for  SH as parameter 6:
+DEFINE password_sh         = &6
+PROMPT
+PROMPT specify password for  BI as parameter 7:
+DEFINE password_bi         = &7
+PROMPT
+PROMPT specify default tablespace as parameter 8:
+DEFINE default_ts          = &8
+PROMPT
+PROMPT specify temporary tablespace as parameter 9:
+DEFINE temp_ts             = &9
+PROMPT
+PROMPT specify log file directory (including trailing delimiter) as parameter 10:
+DEFINE logfile_dir         = &10
+PROMPT
+PROMPT specify connect string as parameter 11:
+DEFINE connect_string     = &11
 PROMPT
 PROMPT Sample Schemas are being created ...
 PROMPT
@@ -141,7 +138,7 @@ DEFINE vrs = v3
 
 host mkdir &&logfile_dir
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 
 DROP USER hr CASCADE;
 DROP USER oe CASCADE;
@@ -150,43 +147,43 @@ DROP USER ix CASCADE;
 DROP USER sh CASCADE;
 DROP USER bi CASCADE;
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 
 SET SHOWMODE OFF
 
 @__SUB__CWD__/human_resources/hr_main.sql &&password_hr &&default_ts &&temp_ts &&password_sys &&logfile_dir &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 SET SHOWMODE OFF
 
 @__SUB__CWD__/order_entry/oe_main.sql &&password_oe &&default_ts &&temp_ts &&password_hr &&password_sys __SUB__CWD__/order_entry/ &&logfile_dir &vrs &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 
 SET SHOWMODE OFF
 
 @__SUB__CWD__/product_media/pm_main.sql &&password_pm &&default_ts &&temp_ts &&password_oe &&password_sys __SUB__CWD__/product_media/ &&logfile_dir __SUB__CWD__/product_media/ &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 SET SHOWMODE OFF
 
 @__SUB__CWD__/info_exchange/ix_main.sql &&password_ix &&default_ts &&temp_ts &&password_sys &&logfile_dir &vrs &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 SET SHOWMODE OFF
 
 @__SUB__CWD__/sales_history/sh_main &&password_sh &&default_ts &&temp_ts &&password_sys __SUB__CWD__/sales_history/ &&logfile_dir &vrs &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 SET SHOWMODE OFF
 
 @__SUB__CWD__/bus_intelligence/bi_main &&password_bi &&default_ts &&temp_ts &&password_sys &&password_oe &&password_sh &&logfile_dir &vrs &&connect_string
 
-CONNECT system/&&password_system@&&connect_string
+CONNECT admin/"&&password_admin"@&&connect_string
 
 SPOOL OFF
 
 DEFINE veri_spool = &&logfile_dir.mkverify_&vrs..log
 
-@__SUB__CWD__/mkverify &&password_system &veri_spool &&connect_string
+@__SUB__CWD__/mkverify &&password_admin &veri_spool &&connect_string
 
